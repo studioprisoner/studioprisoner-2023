59fa3579d6f75b1658c157eae6b4cc046c3e60ad
Josh Illichmann <josh@studioprisoner.com>
2023-01-12 17:37:03 +1100

    fix type errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import { get } from '@vercel/edge-config';
+import { redirect } from 'next/navigation';
 import { FaTwitter, FaGithub, FaLinkedin, FaInstagram, FaMastodon } from 'react-icons/fa'
 
 
-function LinkCard({title, href, image}: { href: string; title: string; image?: string }) {
+function LinkCard({title, href, image}: { title: string; href: string; image?: string }) {
   return (
     <a href={href} className='flex items-center p-1 w-full rounded-md shadow-sm hover:scale-105 bg-white transition-all border border-gray-300 mb-3 text-black max-w-3xl'>
       <div className="flex text-center w-full">
@@ -24,8 +25,31 @@ function LinkCard({title, href, image}: { href: string; title: string; image?: s
   )
 }
 
+interface Data {
+  name: string;
+  avatar: string;
+  links: Link[];
+  socials: Social[]
+  }
+
+interface Link {
+  title: string;
+  href: string;
+  image?: string; 
+}
+
+interface Social {
+  name: string;
+  href: string;
+}
+
 export default async function HomePage() {
-  const data = await get('studiolinks');
+  const data: Data | undefined  = await get('studiolinks');
+
+  if (!data) {
+    redirect ('https://joshillichmann.com')
+  }
+
   return (
     <div className="flex flex-col items-center mx-auto w-full justify-center mt-16 px-8 text-white">
       <Image 
@@ -40,12 +64,21 @@ export default async function HomePage() {
         <LinkCard key={link.href} {...link} />
       ))}
       <div className='flex items-center gap-4 mt-8'>
-        {data.socials.map((link) => {
-          if (link.href.includes('twitter')) {
-            return <FaTwitter className='w-8 h-8' />
+        {data.socials.map((social) => {
+          if (social.href.includes('twitter')) {
+            return <FaTwitter className='w-8 h-8' key={social.href} />
           }
-          if (link.href.includes('instagram')) {
-            return <FaInstagram className='w-8 h-8'/>
+          if (social.href.includes('instagram')) {
+            return <FaInstagram className='w-8 h-8' key={social.href}/>
+          }
+          if (social.href.includes('linkedin')) {
+            return <FaLinkedin className='w-8 h-8' key={social.href}/>
+          }
+          if (social.href.includes('github')) {
+            return <FaGithub className='w-8 h-8' key={social.href}/>
+          }
+          if (social.href.includes('subculture')) {
+            return <FaMastodon className='w-8 h-8' key={social.href}/>
           }
         })}
       </div>
